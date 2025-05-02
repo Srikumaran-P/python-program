13e9d4d592666c6e5126b5e07e8a1b7250858b4c
Srikumaran-P <srikumaranp33@gmail.com>
2025-05-02 12:25:45 +0530

    Create binary_order_tree_order_traversal.py

---
+++ day9/binary_order_tree_order_traversal.py
@@ -0,0 +1,38 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def levelOrderBottom(self, root):
+        
+        if root==None:
+
+            return []
+
+        result=[]
+        queue=[root]
+
+        while queue:
+
+            level=len(queue)
+            node_temp=[]
+
+            for i in range(level):
+
+                node=queue.pop(0)
+                node_temp.append(node.val)
+
+                if node.left!=None:
+
+                    queue.append(node.left)
+
+                if node.right!=None:
+
+                    queue.append(node.right)
+
+            result.append(node_temp)
+        
+        return result[::-1]
+        
